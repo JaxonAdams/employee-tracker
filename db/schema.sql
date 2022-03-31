f67ffd7cc3bed11bda8b85f4b09fe6516c442134
Jaxon Adams <jadamsresume1@gmail.com>
2022-03-31 10:54:09 -0600

    added functionality to view all employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employees;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -12,4 +13,16 @@ CREATE TABLE roles (
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    department_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,15 @@ INSERT INTO department (name)
 VALUES
     ('human_resources'),
     ('sales'),
-    ('accounting');
+    ('accounting'),
+    ('management');
 INSERT INTO roles (title, salary, department_id)
 VALUES
     ('salesman', 50, 2),
-    ('accountant', 60, 3);
+    ('accountant', 60, 3),
+    ('manager', 70, 4);
+INSERT INTO employees (first_name, last_name, department_id, role_id, manager_id)
+VALUES
+    ('jaxon', 'adams', 4, 3, null),
+    ('paige', 'davis', 2, 1, 1),
+    ('brea', 'adams', 3, 2, 1);
